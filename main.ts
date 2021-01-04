3d088c44735397b8909ed199b91fc5730037c121
cakepower <32028127+cakepower@users.noreply.github.com>
2021-01-04 20:12:49 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,20 +4,18 @@ strip.setBrightness(20)
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
 basic.pause(100)
 basic.forever(function () {
-    for (let index = 0; index < 50; index++) {
-        for (let index2 = 0; index2 <= 7; index2++) {
-            strip.setPixelColor(index2, neopixel.colors(NeoPixelColors.White))
-            strip.show()
-            basic.pause(1000)
-            strip.setPixelColor(index2, neopixel.colors(NeoPixelColors.Black))
-            strip.show()
-            count = index2 + 1
-            if (count == 8) {
-                count = 0
-            }
-            strip.setPixelColor(count, neopixel.colors(NeoPixelColors.White))
-            strip.show()
-            basic.pause(1000)
+    for (let index2 = 0; index2 <= 7; index2++) {
+        strip.setPixelColor(index2, neopixel.colors(NeoPixelColors.White))
+        strip.show()
+        basic.pause(1000)
+        strip.setPixelColor(index2, neopixel.colors(NeoPixelColors.Black))
+        strip.show()
+        count = index2 + 1
+        if (count == 8) {
+            count = 0
         }
+        strip.setPixelColor(count, neopixel.colors(NeoPixelColors.White))
+        strip.show()
+        basic.pause(1000)
     }
 })
