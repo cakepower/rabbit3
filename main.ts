e9273e1b309400fcf90df52721ebd16874b9deb2
cakepower <32028127+cakepower@users.noreply.github.com>
2022-11-17 10:38:04 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,9 +1,4 @@
-let count = 0
-let strip = neopixel.create(DigitalPin.P11, 8, NeoPixelMode.RGB)
-strip.setBrightness(20)
-strip.showColor(neopixel.colors(NeoPixelColors.Black))
-basic.pause(100)
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     for (let index2 = 0; index2 <= 7; index2++) {
         strip.setPixelColor(index2, neopixel.colors(NeoPixelColors.White))
         strip.show()
@@ -19,3 +14,9 @@ basic.forever(function () {
         basic.pause(1000)
     }
 })
+let count = 0
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P13, 8, NeoPixelMode.RGB)
+strip.setBrightness(20)
+strip.showColor(neopixel.colors(NeoPixelColors.Black))
+basic.pause(100)
